b5e252bdd2c2d477e9d974e731cf71999ed8ca57
Pouya <devpouya@gmail.com>
2022-01-23 10:52:49 +0100

    forgot new files

---
+++ solbond/programs/solbond/src/instructions/saber_provide.rs
@@ -0,0 +1,162 @@
+use anchor_lang::prelude::*;
+use anchor_lang::solana_program::program_option::COption;
+use anchor_spl::token::{self, Mint, Token, TokenAccount};
+use crate::state::{BondPoolAccount, InvariantPoolAccount};
+use stable_swap_anchor::*;
+use stable_swap_anchor::{Deposit, SwapToken, SwapUserContext};
+use anchor_lang::solana_program::system_program;
+use stable_swap_anchor::StableSwap;
+
+//use amm::{self, Tickmap, State, Pool, Tick, Position, PositionList};
+
+#[derive(Accounts)]
+// #[instruction(amount)]
+#[instruction(
+    _bump_bond_pool_account: u8,
+    token_a_amount: u64,
+    token_b_amount: u64,
+    min_mint_amount: u64,
+)]
+pub struct SaberLiquidityInstruction<'info> {
+
+    //pub user: AccountInfo<'info>,
+    /// The "A" token of the swap.
+    //pub input_a: AccountInfo<'info>,
+    /// The "B" token of the swap.
+    //pub input_b: AccountInfo<'info>,
+    /// The pool mint of the swap.
+    /// 
+    /// 
+    #[account(signer)]
+    pub initializer: AccountInfo<'info>,
+
+    #[account(mut)]
+    pub bond_pool_currency_token_mint: AccountInfo<'info>,
+
+    #[account(mut)]
+    pub pool_mint: AccountInfo<'info>,
+    /// The output account for LP tokens.
+    /// 
+    #[account(mut)]
+    pub output_lp: AccountInfo<'info>,
+
+    // user block
+    //#[account(mut)]
+    pub token_program: AccountInfo<'info>,
+    /// The authority of the swap.
+    #[account(mut)]
+    pub swap_authority: AccountInfo<'info>,
+    /// The authority of the user.
+    //#[account(mut, signer)]
+    #[account(
+        mut,
+        seeds=[bond_pool_currency_token_mint.key.as_ref(), b"bondPoolAccount1"],
+        bump = _bump_bond_pool_account
+    )]
+    pub user_authority: AccountInfo<'info>,
+    /// The swap.
+    //#[account(mut)]
+    pub swap: AccountInfo<'info>,
+    /// The clock
+    //#[account(mut)]
+    pub clock: AccountInfo<'info>,
+
+    // input block
+    #[account(mut)]
+    pub user_a: AccountInfo<'info>,
+    /// The token account for the pool's reserves of this token.
+    #[account(mut)]
+    pub reserve_a: AccountInfo<'info>,
+    #[account(mut)]
+    pub user_b: AccountInfo<'info>,
+    /// The token account for the pool's reserves of this token.
+    #[account(mut)]
+    pub reserve_b: AccountInfo<'info>,
+
+
+    
+    pub saber_swap_program: Program<'info, StableSwap>,
+    pub system_program: AccountInfo<'info>,
+    
+
+
+}
+
+/*
+    Based on the portfolio and weights, calculate how much to re-distribute into each pool
+*/
+// // TODO: Replace everything by decimals?
+// pub fn calculate_amount_per_pool(x: u64) -> [u64; 5] {
+//
+//     let default_pay_in_amount: u64 = x / 5;
+//
+//     return [default_pay_in_amount, default_pay_in_amount, default_pay_in_amount, default_pay_in_amount, default_pay_in_amount];
+// }
+
+/**
+    Deposit reserve to pools.
+    All the Solana tokens that are within the reserve,
+    are now put into
+    Frontend should be respondible for creating all the required token accounts!
+ */
+pub fn handler(
+    ctx: Context<SaberLiquidityInstruction>,
+    _bump_bond_pool_account: u8,
+    token_a_amount: u64,
+    token_b_amount: u64,
+    min_mint_amount: u64,
+) -> ProgramResult {
+    msg!("Depositing reserve to pools!");
+
+    let user_context: SwapUserContext = SwapUserContext {
+        token_program: ctx.accounts.token_program.to_account_info(),
+        swap_authority: ctx.accounts.swap_authority.to_account_info(),
+        user_authority: ctx.accounts.user_authority.to_account_info(),
+        swap: ctx.accounts.swap.to_account_info(),
+    };
+
+    let input_a: SwapToken = SwapToken {
+        user: ctx.accounts.user_a.to_account_info(),
+        reserve: ctx.accounts.reserve_a.to_account_info(),
+    };
+
+    let input_b: SwapToken = SwapToken {
+        user: ctx.accounts.user_b.to_account_info(),
+        reserve: ctx.accounts.reserve_b.to_account_info(),
+    };
+
+    let deposit_context: Deposit = Deposit {
+       user: user_context,
+       input_a: input_a,
+       input_b: input_b,
+       pool_mint: ctx.accounts.pool_mint.to_account_info(),
+       output_lp: ctx.accounts.output_lp.to_account_info(),
+    };
+    let saber_swap_program = ctx.accounts.saber_swap_program.to_account_info();
+
+
+    stable_swap_anchor::deposit(
+        CpiContext::new(
+            saber_swap_program,
+            deposit_context,
+        ),
+        token_a_amount,
+        token_b_amount,
+        min_mint_amount,
+    )?;
+
+
+    // // Calculate how much currency is in the bond
+    // let available_currency: u64 = ctx.accounts.bond_pool_currency_account.amount;
+    //
+    // // For now, assume we provide the same amount of liquidity to all pools
+    // // So we don't have to calculate the weightings
+    // let fraction_per_pool = calculate_amount_per_pool(available_currency);
+    //
+    // // Make swaps, and deposit this much to the pool
+    // for i in 0..fraction_per_pool.len() {
+    //
+    // }
+
+    Ok(())
+}
---
+++ solbond/tests/saber-only.spec.ts
@@ -0,0 +1,352 @@
+/**
+ * Test to test all functionality of the invariant program that we have here
+ *
+ * This includes the following functionality in this order
+ * - create pool
+ * - provide liquidity
+ * - make a trade as a third party
+ * - claim fees
+ * - close position
+ *
+ * Some functionality will deviate between devnet and localnet, thus we have created two different tests
+ */
+ import {TOKEN_PROGRAM_ID, u64} from "@solana/spl-token";
+ import * as anchor from "@project-serum/anchor";
+ import {Solbond} from '../target/types/solbond';
+
+ import {BN, Provider} from "@project-serum/anchor";
+ import {Keypair, PublicKey, SystemProgram, Transaction} from "@solana/web3.js";
+ import {airdropAdmin, getSolbondProgram, MOCK} from "@qpools/sdk";
+ import {QPair, QPoolsAdmin} from "@qpools/admin-sdk/lib/qpools-admin-sdk/src";
+ import {NETWORK} from "@qpools/sdk/lib/cluster";
+ import {Token} from "@solana/spl-token";
+ import {assert} from "chai";
+ import {delay} from "@qpools/sdk/lib/utils";
+ import {
+    BondPoolAccount,
+    createAssociatedTokenAccountSendUnsigned,
+    createMint,
+    getPayer,
+} from "@qpools/sdk";
+import {Program, utils, Wallet, web3} from "@project-serum/anchor";
+import {LAMPORTS_PER_SOL} from "@solana/web3.js"
+import type {  TransactionInstruction } from "@solana/web3.js";
+import {Connection} from "@solana/web3.js";
+import type { IExchange } from "@saberhq/stableswap-sdk";
+import {
+  calculateVirtualPrice,
+  deployNewSwap,
+  loadExchangeInfo,
+  parseEventLogs,
+  StableSwap,
+  findSwapAuthorityKey,
+  SWAP_PROGRAM_ID,
+} from "@saberhq/stableswap-sdk";
+import {
+    createAssociatedTokenAccountUnsigned,
+    getAssociatedTokenAddressOffCurve,
+
+} from "@qpools/sdk";
+import {IDL} from "../target/types/solbond";
+
+//import {
+//  SPLToken,
+//  Token as SToken,
+//} from "@saberhq/token-utils";
+
+
+
+ describe('saber-devnet', () => {
+    
+     // Get connection and provider
+    const provider = Provider.local("https://api.devnet.solana.com");
+    const connection = provider.connection;
+    console.log("here")
+
+    console.log('provider ', provider);
+    // @ts-expect-error
+    const genericPayer = provider.wallet.payer as Keypair;
+    //const genericWallet = provider.wallet;
+    const mintAuthority = genericPayer;
+    //// Generate the users
+
+    //const currencyOwner = airdropAdmin;
+    // const solbondProgram = anchor.workspace.Solbond as Program<Solbond>;
+    //const solbondProgram = anchor.workspace.solbondProgram.program
+    //console.log("solbondPorgram ID ", solbondProgram.toString());
+    //
+    let programAddress = "3vTbhuwJwR5BadSH9wt29rLf91S57x31ynQZJpG9cf7E";
+
+    const programId = new anchor.web3.PublicKey(programAddress);
+    const solbondProgram: any = new anchor.Program(
+        IDL,
+        programId,
+        provider,
+    );
+
+    //const solbondProgram = getSolbondProgram(connection, provider, NETWORK.DEVNET);
+
+    //    
+    //let tokenPool: SPLToken;
+    //let userPoolAccount: PublicKey;
+    //// Tokens swapped
+    let mintA: Token;
+    let mintB: Token;
+    let tokenAccountA: PublicKey;
+    let tokenAccountB: PublicKey;
+    //// Admin fee accounts
+    let adminFeeAccountA: PublicKey;
+    let adminFeeAccountB: PublicKey;
+    //// Stable swap
+    let exchange: IExchange;
+    let swapAccount;
+    let swapAuthority;
+    let stableSwap: StableSwap;
+    let fetchedStableSwap: StableSwap;
+    let stableSwapAccount : Keypair;
+    //stableSwapAccount=   Keypair.generate();
+    let stableSwapProgramId: PublicKey;
+    let stableSwapState;
+    //stableSwapProgramId = SWAP_PROGRAM_ID;
+    //console.log("Solbond Program");
+    //console.log(solbondProgram.programId.toString());
+    //console.log("Invariant Program");
+    const AMP_FACTOR = 100;
+    let QPTokenMint: Token;
+    let qPoolCurrencyAccount;
+
+    let USDC_USDT_pubkey: PublicKey;
+    //let market: QPoolsAdmin;
+    //let currencyMint: Token;
+    // 
+
+    before(async () => {
+        console.log("swapprogramid")
+        stableSwapProgramId = new PublicKey(
+            "SSwpkEEcbUqx4vtoEByFjSkhKdCT862DNVb52nZg1UZ"
+        );
+        stableSwapAccount = Keypair.generate()
+        USDC_USDT_pubkey = new PublicKey("VeNkoB1HvSP6bSeGybQDnx9wTWFsQb2NBCemeCDSuKL");
+
+        console.log("create QPT")
+        QPTokenMint = await createMint(
+            provider,
+            genericPayer,
+            mintAuthority.publicKey,
+            9
+        );
+        //const qpt = new Token(connection, new PublicKey(MOCK.DEV.USDC), TOKEN_PROGRAM_ID, genericPayer)
+    
+
+        console.log('creating accounts')
+        qPoolCurrencyAccount = await QPTokenMint.createAccount(mintAuthority.publicKey)
+        console.log('qpoolcurrency acc ', qPoolCurrencyAccount.toString())
+        
+        
+
+    })
+    it("loadStableSwap", async () => {
+        fetchedStableSwap = await StableSwap.load(
+         connection,
+         USDC_USDT_pubkey,
+         stableSwapProgramId
+       );
+       console.log("loaded")
+       assert.ok(fetchedStableSwap.config.swapAccount.equals(
+         USDC_USDT_pubkey)
+       );
+       swapAccount = fetchedStableSwap.config.swapAccount
+       swapAuthority = fetchedStableSwap.config.authority
+       const { state } = fetchedStableSwap;
+       console.log(state);
+       stableSwapState = state
+       
+       mintA = new Token(connection,state.tokenA.mint, TOKEN_PROGRAM_ID, genericPayer);
+       mintB = new Token(connection,state.tokenB.mint, TOKEN_PROGRAM_ID, genericPayer);
+
+       
+
+       console.log("mint A ", mintA);
+
+       // assert.ok(state.tokenA.adminFeeAccount.equals(adminFeeAccountA));
+       // assert.ok(state.tokenB.adminFeeAccount.equals(adminFeeAccountB));
+       // assert.ok(state.tokenA.reserve.equals(tokenAccountA));
+       // assert.ok(state.tokenB.reserve.equals(tokenAccountB));
+       // assert.ok(state.tokenA.mint.equals(mintA.publicKey));
+       // assert.ok(state.tokenB.mint.equals(mintB.publicKey));
+       // assert.ok(state.poolTokenMint.equals(tokenPool.publicKey));
+       // assert.ok(state.initialAmpFactor.toNumber() == AMP_FACTOR);
+       // assert.ok(state.targetAmpFactor.toNumber() == AMP_FACTOR);
+       
+    });
+
+    
+
+    it("depositToSaberWithCPI", async() => {
+
+        let amountTokenA = new u64(LAMPORTS_PER_SOL);
+        let amountTokenB = new u64(LAMPORTS_PER_SOL);
+
+        let minMintAmount = new u64(0);
+
+
+        let tokenAMint = stableSwapState.tokenA.mint
+        let tokenBMint = stableSwapState.tokenB.mint
+
+        let poolTokenMint = stableSwapState.poolTokenMint
+        let poolMint = new Token(connection,poolTokenMint, TOKEN_PROGRAM_ID, genericPayer);
+
+
+
+        console.log(tokenAMint.toString())
+        console.log(tokenBMint.toString())
+        console.log(poolTokenMint.toString())
+
+        //stableSwap.deposit({
+        //    userAuthority: owner.publicKey,
+        //    sourceA: userAccountA,
+        //    sourceB: userAccountB,
+        //    poolTokenAccount: userPoolAccount,
+        //    tokenAmountA: new u64(depositAmountA),
+        //    tokenAmountB: new u64(depositAmountB),
+        //    minimumPoolTokenAmount: new u64(0), // To avoid slippage errors
+        //  })
+        //);
+
+        let [qPoolPDA, bumpqpoolaccount] = await PublicKey.findProgramAddress(
+            [QPTokenMint.publicKey.toBuffer(), Buffer.from(anchor.utils.bytes.utf8.encode("bondPoolAccount1"))],
+            solbondProgram.programId
+        );
+
+        
+        //let swapAuthority = stableSwapState.config.authority
+        //console.log(swapAuthority.toString())
+    
+        const [authority] = await findSwapAuthorityKey(stableSwapState.adminAccount, stableSwapProgramId);
+        console.log("authority ", authority.toString())
+
+        // try {
+        //     let tx = await createAssociatedTokenAccountUnsigned(
+        //         connection,
+        //         stableSwapState.tokenA.mint,
+        //         null,
+        //         qPoolCurrencyAccount,
+        //         provider.wallet
+        //     );
+        //     const sg = await connection.sendTransaction(tx, [genericPayer]);
+        //     await connection.confirmTransaction(sg);
+        //     console.log("Signature for token A is: ", sg);
+        // } catch (e) {
+        //     console.log("Error is: ");
+        //     console.log(e);
+        // }
+
+        //let userAccountA = await getAssociatedTokenAddressOffCurve(stableSwapState.tokenA.mint, qPoolCurrencyAccount);
+        console.log("qpollcurrarr", qPoolCurrencyAccount.toString())
+        let userAccountA = await mintA.createAccount(qPoolCurrencyAccount)
+        console.log("mint A")
+
+        //await mintA.mintTo(userAccountA, genericPayer, [], amountTokenA);
+        // Creating depositor token b account
+
+        //try {
+        //    let tx = await createAssociatedTokenAccountUnsigned(
+        //        connection,
+        //        stableSwapState.tokenB.mint,
+        //        null,
+        //        qPoolCurrencyAccount,
+        //        provider.wallet
+        //    );
+        //    const sg = await connection.sendTransaction(tx, [genericPayer]);
+        //    await connection.confirmTransaction(sg);
+        //    console.log("Signature for token B is: ", sg);
+        //} catch (e) {
+        //    console.log("Error is: ");
+        //    console.log(e);
+        //}
+        let userAccountB = await mintB.createAccount(qPoolCurrencyAccount)
+
+        //let userAccountB = await getAssociatedTokenAddressOffCurve(stableSwapState.tokenB.mint, qPoolCurrencyAccount);
+        try{
+            await mintA.mintTo(userAccountA, genericPayer, [], amountTokenA);
+        } catch (e) {
+            console.log("Error in mint A ", e)
+        }
+        try {
+            await mintB.mintTo(userAccountB, genericPayer, [], amountTokenB);
+        } catch (e) {
+            console.log("Error in mint B ", e)
+        }
+        
+        console.log("mint B")
+
+        let userAccountpoolToken  = await poolMint.createAccount(qPoolCurrencyAccount)
+        //try {
+        //    let tx = await createAssociatedTokenAccountUnsigned(
+        //        connection,
+        //        poolTokenMint,
+        //        null,
+        //        qPoolCurrencyAccount,
+        //        provider.wallet
+        //    );
+        //    const sg = await connection.sendTransaction(tx, [genericPayer]);
+        //    await connection.confirmTransaction(sg);
+        //    console.log("Signature for pool token is: ", sg);
+        //} catch (e) {
+        //    console.log("Error is: ");
+        //    console.log(e);
+        //}
+
+        // let userAccountpoolToken = await getAssociatedTokenAddressOffCurve(poolTokenMint, qPoolCurrencyAccount);
+
+        console.log("swap authority", authority.toString());
+        console.log("pool token Mint", poolTokenMint.toString())
+        console.log("output lp", userAccountpoolToken.toString())
+        console.log("userAuthority, ", qPoolPDA.toString())
+        console.log("swap account", swapAccount.toString())
+        console.log("user A", userAccountA.toString())
+        console.log("reserve A", stableSwapState.tokenA.reserve.toString())
+
+
+        console.log("user B", userAccountB.toString())
+        console.log("reserve B", stableSwapState.tokenB.reserve.toString())
+
+        
+        
+        let finaltx = await solbondProgram.rpc.createLiquidityPositionSaber(
+            new BN(bumpqpoolaccount),
+            new BN(amountTokenA),
+            new BN(amountTokenB),
+            new BN(minMintAmount),
+            {
+                accounts: {
+                    initializer: genericPayer.publicKey,
+                    bondPoolCurrencyTokenMint: QPTokenMint.publicKey,
+                    poolMint: poolTokenMint,
+                    outputLp: userAccountpoolToken,
+                    tokenProgram: TOKEN_PROGRAM_ID,
+                    swapAuthority: swapAuthority,
+                    userAuthority: qPoolPDA,
+                    swap:swapAccount,
+                    clock:web3.SYSVAR_CLOCK_PUBKEY,
+                    userA: userAccountA,
+                    reserveA: stableSwapState.tokenA.reserve,
+                    userB: userAccountB,
+                    reserveB: stableSwapState.tokenB.reserve,    
+                    saberSwapProgram: stableSwapProgramId,
+                    systemProgram: web3.SystemProgram.programId,
+                    // Create liquidity accounts
+                },
+                signers:[genericPayer]
+            }
+        )
+
+        await provider.connection.confirmTransaction(finaltx);
+
+    });
+
+
+
+ 
+ 
+ })
