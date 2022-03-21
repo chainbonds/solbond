a0513659251190a8075a70a5f6f74f0b01b84708
yenicelik <david.yenicelik@gmail.com>
2022-03-21 13:21:47 +0100

    removed USDC Account

--- dapp-nextjs/src/components/tables/ExistingPortfolioTable.tsx
+++ dapp-nextjs/src/components/tables/ExistingPortfolioTable.tsx
@@ -5,7 +5,6 @@ import Image from "next/image";
 import {PositionInfo, registry} from "@qpools/sdk";
 import {useWallet} from "@solana/wallet-adapter-react";
 import {ProtocolType} from "../../../../../qPools-contract/qpools-sdk/lib/types/positionInfo";
-import {UsdValuePosition} from "../../types/UsdValuePosition";
 
 const tableColumns: (string | null)[] = ["Pool", "Assets", "USDC Value", null]
 
